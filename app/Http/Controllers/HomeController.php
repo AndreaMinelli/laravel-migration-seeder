6d7ae62e1142f5969661e9cae3ccc0543d9f9fdb
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-27 17:21:02 +0100

    Aggiunto filtro departure_time

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,9 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $trains = Train::all();
+        $trains = Train::all()
+            ->sortBy('departure_time')
+            ->where('departure_time', '>', date('H:i'));
         return view('home', compact('trains'));
     }
 }
