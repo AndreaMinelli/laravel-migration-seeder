97d60230095938fa7339e56ded8dd97e0af1eb4c
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-27 16:41:29 +0100

    Stampati in pagina i treni

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,37 @@
 
 <body>
     @include('includes.header')
+
+    <main class="bg-dark py-4 min-vh-100">
+        <div class="container">
+            <table class="table text-white text-center">
+                <thead>
+                    <tr>
+                        <th scope="col">Numero Treno</th>
+                        <th scope="col">Stazione di Partenza</th>
+                        <th scope="col">Orario di Partenza</th>
+                        <th scope="col">Stazione di Arrivo</th>
+                        <th scope="col">Orario di Arrivo</th>
+                        <th scope="col">In Orario</th>
+                        <th scope="col">Cancellato</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($trains as $train)
+                        <tr>
+                            <th scope="row">{{ $train->train_number }}</th>
+                            <td>{{ $train->departure_station }}</td>
+                            <td>{{ $train->departure_time }}</td>
+                            <td>{{ $train->arrival_station }}</td>
+                            <td>{{ $train->arrival_time }}</td>
+                            <td>{{ $train->in_time }}</td>
+                            <td>{{ $train->deleted }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </main>
 </body>
 
 </html>
